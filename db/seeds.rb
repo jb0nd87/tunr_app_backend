a015f5bc29435fac11dc270fb0b419d55c540e5b
jb0nd87 <68964150+jb0nd87@users.noreply.github.com>
2020-11-16 09:50:53 -0800

    tunr backend seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Song.create(
+    title: Faker::Hipster.sentence(word_count: 3),
+    artist_name: Faker::Team.name,
+  )
+end
+puts 'seeded data'
